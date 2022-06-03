e04b02541e7d2a041588e9b400bbb5e0e834cf4f
KeerthishankarPunchithaya <punchithayakeerthishankar@gmail.com>
2022-06-03 21:33:05 +0530

    modifications done,need changes

--- app/src/main/java/com/example/ecomplaint/MainActivity.java
+++ app/src/main/java/com/example/ecomplaint/MainActivity.java
@@ -15,26 +15,18 @@ package com.example.ecomplaint;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.util.Patterns;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -43,8 +35,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -71,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
 
         //rootNode = FirebaseDatabase.getInstance();
 
-        editTextName=findViewById(R.id.editTextName);
+        editTextName=findViewById(R.id.username);
 
         editTextPassword=findViewById(R.id.editTextPassword);
 
